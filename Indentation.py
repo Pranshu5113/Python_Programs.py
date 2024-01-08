f865280f75876216b3ee1df5fca57d8d1b75136b
Pranshu Singh Patel <116775668+Pranshu5113@users.noreply.github.com>
2024-01-08 22:21:38 +0530

    Add files via upload

---
+++ Indentation.py
@@ -0,0 +1,14 @@
+
+# This is a comment 
+# Here we have define 
+# about indentation in python !!! 
+
+a = 6
+b = 4
+
+if 6 > 4 :
+    print(" 6 is greater than 4 ") 
+
+else :
+    print(" 4 is greater than 6 ") 
+       
