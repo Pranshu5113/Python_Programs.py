10f3900b7df2dbd307277dbc9517be1facab4106
Pranshu Singh Patel <116775668+Pranshu5113@users.noreply.github.com>
2024-05-17 23:25:56 +0530

    Add files via upload

---
+++ compare.py
@@ -0,0 +1,11 @@
+
+
+# compare the value of a and b in python using if else condition #
+
+a = input("Enter the value of a :")
+b = input("Enter the value of b :")
+
+if(a != b):
+    print("a is not Equal")
+else:
+    print("a is equal to b")
